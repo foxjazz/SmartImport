2a475c7b506cf9ee4b00b5abd37ab7545e9df617
jdickinson <jdickinson@statebridgecompany.com>
2017-05-16 10:12:04 -0600

    Add project files.

---
+++ SmartImport/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SmartImport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This project moves data from csv files into the import tables listed in the ImportSource table.
+
+            //Read ImportSource table for parameters
+
+
+        }
+    }
+}
